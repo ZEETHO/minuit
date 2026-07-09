1663ebef0353161bd909b1939578b4f0b422e0b8
ZEETHO <141181330+ZEETHO@users.noreply.github.com>
2026-07-09 10:10:50 -0400

    Add in-app install guide (iOS+Android) modal

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minuit-v2';
+const CACHE = 'minuit-v3';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
