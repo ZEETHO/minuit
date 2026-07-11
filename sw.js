0af21f9b0ba4c9c1778554a7c838bc6ccfdcbe05
ZEETHO <141181330+ZEETHO@users.noreply.github.com>
2026-07-11 14:44:28 -0400

    Add Mario/Arcade mode toggle with 5-language switcher (EN/FR/ES/JA/ZH), keep sleek French mode

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minuit-v14';
+const CACHE = 'minuit-v15';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
