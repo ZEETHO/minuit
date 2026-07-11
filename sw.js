6d1f898defb808f1da7fd30fcfed40aca49a5a51
ZEETHO <141181330+ZEETHO@users.noreply.github.com>
2026-07-11 13:54:52 -0400

    Add language translator (MyMemory free API, 8 langs, swap, speak output)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'minuit-v12';
+const CACHE = 'minuit-v13';
 const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
