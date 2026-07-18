e24a0b1ae1bd562b308ae00559d306b61734b6a3
ZEETHO <141181330+ZEETHO@users.noreply.github.com>
2026-07-18 12:33:19 -0400

    Add 10 features: streak shield, blitz, phrasebook, SRS review, Mario coins, mic grade, ambient, share card, profiles, landing+QR (v16)

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = 'minuit-v15';
-const ASSETS = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE = 'minuit-v16';
+const ASSETS = ['./', './index.html', './landing.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
